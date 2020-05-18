216dae8deca8e582899936fd47e82ad3927888ea
Jaejong Heo <hapsoa@gmail.com>
2020-05-19 01:03:13 +0900

    update timdDictAboutKeywordObjectDictIncludingNer

--- src/ToRefine/7.makeSpacyNerPropertyOfKeywordObjectDict.py
+++ src/ToRefine/7.makeSpacyNerPropertyOfKeywordObjectDict.py
@@ -8,23 +8,29 @@ from nltk import pos_tag
 # import nltk
 # from nltk import Tree
 import spacy
-import en_core_web_sm
+import en_core_web_sm  # python -m spacy download en_core_web_sm
 
 # test file path
 # keywordObjectDictTotalTimeFilePath = '../../test-data/keywordObjectDictTotalTimeForTest.json'
+# timeDictAboutKeywordObjectDictFilePath = '../../test-data/timeDictAboutKeywordObjectDictForTest.json'
 # invertedIndexFilePath = '../../test-data/invertedIndexForTest.json'
 # combinedLemmatizedTextFilePath = '../../test-data/combinedLemmatizedTextsForTest.json'
-# writingKeywordObjectDictIncludingNer = '../../test-data/keywordObjectDictIncludingNerForTest.json'
+# writingKeywordObjectDictIncludingNerPath = '../../test-data/keywordObjectDictIncludingNerForTest.json'
+# writingTimeDictAboutKeywordObjectDictIncludingNerPath = '../../test-data/timeDictAboutKeywordObjectDictIncludingNerForTest.json'
 
 # real file path
 keywordObjectDictTotalTimeFilePath = '../../result-data/keywordObjectDictTotalTime.json'
+timeDictAboutKeywordObjectDictFilePath = '../../result-data/timeDictAboutKeywordObjectDict.json'
 invertedIndexFilePath = '../../result-data/invertedIndex.json'
 combinedLemmatizedTextFilePath = '../../result-data/combinedLemmatizedTexts.json'
-writingKeywordObjectDictIncludingNer = '../../result-data/keywordObjectDictIncludingNer.json'
+writingKeywordObjectDictIncludingNerPath = '../../result-data/keywordObjectDictIncludingNer.json'
+writingTimeDictAboutKeywordObjectDictIncludingNerPath = '../../result-data/timeDictAboutKeywordObjectDictIncludingNer.json'
 
 # read data
 keywordObjectDictTotalTime = json.load(codecs.open(
     keywordObjectDictTotalTimeFilePath, 'r', 'utf-8-sig'))
+timeDictAboutKeywordObjectDict = json.load(codecs.open(
+    timeDictAboutKeywordObjectDictFilePath, 'r', 'utf-8-sig'))
 invertedIndex = json.load(codecs.open(
     invertedIndexFilePath, 'r', 'utf-8-sig'))
 combinedLemmatizedTexts = json.load(codecs.open(
@@ -42,16 +48,27 @@ for (keyword, postings) in invertedIndex.items():
     # if the keyword has NE, put NE
     # else if the keyword don't have NE, put POS of the keyword
     namedEntities = nlp(document)
-    # print('namedEntities', namedEntities)
+    namedEntityResult = None
     for namedEntity in namedEntities.ents:
+        # if namedEntity.text == 'zelenskiy':
+        #     print(namedEntity.text, namedEntity.label_)
         if regexOfKeyword.search(namedEntity.text) != None:
-            keywordObjectDictTotalTime[keyword]['ner'] = namedEntity.label_
+            namedEntityResult = namedEntity.label_
             break
-    if 'ner' not in keywordObjectDictTotalTime[keyword]:
-        keywordObjectDictTotalTime[keyword]['ner'] = pos_tag([keyword])[0][1]
+    if namedEntityResult == None:
+        namedEntityResult = pos_tag([keyword])[0][1]
+
+    keywordObjectDictTotalTime[keyword]['ner'] = namedEntityResult
+
+    # for each timeUnit,
+    for (time, keywordObjectDict) in timeDictAboutKeywordObjectDict.items():
+        if keyword in keywordObjectDict:
+            keywordObjectDict[keyword]['ner'] = namedEntityResult
 
 
 # write keywordObjectDictIncludingNER
-with open(writingKeywordObjectDictIncludingNer, 'w', encoding='UTF-8-sig') as outfile:
+with open(writingKeywordObjectDictIncludingNerPath, 'w', encoding='UTF-8-sig') as outfile, open(writingTimeDictAboutKeywordObjectDictIncludingNerPath, 'w', encoding='UTF-8-sig') as outfile2:
     outfile.write(json.dumps(
         keywordObjectDictTotalTime, ensure_ascii=False))
+    outfile2.write(json.dumps(
+        timeDictAboutKeywordObjectDict, ensure_ascii=False))
