025a63b53e2c30696976214b02a64e827eba7047
Jaejong Heo <hapsoa@gmail.com>
2020-05-23 15:45:21 +0900

    update flask server

--- src/FlaskServer/app.py
+++ src/FlaskServer/app.py
@@ -14,9 +14,15 @@ cors = CORS(app)
 
 # real files
 timeDictAboutKeywordObjectDictFilePath = '../../lda-ner-result-data/timeDictAboutKeywordObjectDictIncludingNer.json'
+keywordRelationMatrixTotalTimeFilePath = '../../lda-ner-result-data/keywordRelationMatrixTotalTime.json'
+alphabetIndexDictAboutKeywordFilePath = '../../lda-ner-result-data/alphabetIndexDictAboutKeyword.json'
 
 timeDictAboutKeywordObjectDict = json.load(codecs.open(
     timeDictAboutKeywordObjectDictFilePath, 'r', 'utf-8-sig'))
+keywordRelationMatrixTotalTime = json.load(codecs.open(
+    keywordRelationMatrixTotalTimeFilePath, 'r', 'utf-8-sig'))
+alphabetIndexDictAboutKeyword = json.load(codecs.open(
+    alphabetIndexDictAboutKeywordFilePath, 'r', 'utf-8-sig'))
 
 
 @app.route('/', methods=['POST'])
@@ -30,6 +36,16 @@ def post():
 @app.route('/time-dict-about-keyword-object-dict', methods=['GET'])
 def getTimeDictAboutKeywordObjectDict():
     return jsonify(timeDictAboutKeywordObjectDict)
+
+
+@app.route('/keyword-relation-matrix-total-time', methods=['GET'])
+def getKeywordRelationMatrixTotalTime():
+    return jsonify(keywordRelationMatrixTotalTime)
+
+
+@app.route('/alphabet-index-dict-about-keyword', methods=['GET'])
+def getAlphabetIndexDictAboutKeyword():
+    return jsonify(alphabetIndexDictAboutKeyword)
 
 
 if __name__ == '__main__':
--- src/tests/testIntersectOfBooleanRetrieval.ts
+++ src/tests/testIntersectOfBooleanRetrieval.ts
@@ -1,37 +1,41 @@
-import { InvertedIndex } from '../ToRefine/refiningInterfaces';
+import { InvertedIndex } from "../RefineAboutLdaNer/refiningInterfaces";
 
 // read invertedIndex.json
-const invertedIndex: InvertedIndex = require('../../test-data/invertedIndexForTest.json');
+const invertedIndex: InvertedIndex = require("../../test-data/invertedIndexForTest.json");
 
 // read vectors.json
-const documentVectorsFilePath: string = '../../test-data/documentVectorsForTest.json';
+const documentVectorsFilePath: string =
+  "../../test-data/documentVectorsForTest.json";
 const documentVectors: number[][] = require(documentVectorsFilePath);
 
 // query keywords (vector)
-const keywordsForQuery: string[] = ['from', 'common'];
+const keywordsForQuery: string[] = ["from", "common"];
 
 // boolean retrieval intersection of multiple keywords
 // then postings will be retrieved.
-const retrievedDocuments = intersect2WordsOfBooleanRetrieval(keywordsForQuery, invertedIndex);
-console.log('retrievedDocuments', retrievedDocuments);
-
-
-
+const retrievedDocuments = intersect2WordsOfBooleanRetrieval(
+  keywordsForQuery,
+  invertedIndex
+);
+console.log("retrievedDocuments", retrievedDocuments);
 
 /**
  * intersect with 2 keywords in Boolean Retrieval
  * @param keywords
  * @param innerInvertedIndex
  */
-function intersect2WordsOfBooleanRetrieval(keywords: string[], innerInvertedIndex: InvertedIndex): number[] {
+function intersect2WordsOfBooleanRetrieval(
+  keywords: string[],
+  innerInvertedIndex: InvertedIndex
+): number[] {
   // keyword1's postingsList
   const postingsList1: number[] = innerInvertedIndex[keywords[0]];
 
   // keyword2's postingsList
   const postingsList2: number[] = innerInvertedIndex[keywords[1]];
 
-  console.log('postingsList1', postingsList1);
-  console.log('postingsList2', postingsList2);
+  console.log("postingsList1", postingsList1);
+  console.log("postingsList2", postingsList2);
 
   const answer: number[] = [];
 
@@ -54,10 +58,13 @@ function intersect2WordsOfBooleanRetrieval(keywords: string[], innerInvertedInde
 
 // TODO
 /**
- * intersect with multiple keywords of Boolean Retrieval 
+ * intersect with multiple keywords of Boolean Retrieval
  * @param keywords
  * @param innerInvertedIndex
  */
-function intersectOfBooleanRetrieval(keywords: string[], innerInvertedIndex: InvertedIndex): number[] {
+function intersectOfBooleanRetrieval(
+  keywords: string[],
+  innerInvertedIndex: InvertedIndex
+): number[] {
   return [];
 }
